cbf3a4fdf2a7a2b995121c8720535fb2ccc1c285
Vuyisile <vtlehola23@gmail.com>
2024-01-18 07:47:37 -0800

    solve empty argument edge case

--- operationsFunctions.c
+++ operationsFunctions.c
@@ -36,6 +36,11 @@ void push(stack_t **stack, unsigned int line_num, int n)
 		fprintf(stderr, "L%d: usage: push integer", line_num);
 		exit(EXIT_FAILURE);
 	}
+	if (!n)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_num);
+		 exit(EXIT_FAILURE);
+	}
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
 		exit(EXIT_FAILURE);
